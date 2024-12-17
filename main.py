0804dff8e8419290b07f2e2b0535a031260f80e3
ShadeC0der <cguti3rrezm@gmail.com>
2024-12-17 17:06:00 -0300

    add: Configuracion de conexion a base de datos

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ Este es el archivo principal del sistema de reservas 'Viajes Aventura'.
 Se utiliza como punto de entrada al programa.
 """
 
+from config.database import create_connection
+
 def main():
     """
-    Función principal que inicia la ejecución del programa.
+    Función principal que inicia la ejecución del programa y prueba la conexión a la base de datos.
     """
-    print('Hola Mundo')
+    print("Iniciando el sistema de reservas 'Viajes Aventura'...")
+
+    # Probar la conexión a la base de datos
+    create_connection()
 
 if __name__ == "__main__":
     main()
